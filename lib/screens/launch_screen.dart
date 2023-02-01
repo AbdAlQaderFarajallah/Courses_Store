dc99fe483541e4d78b289d63e650ccf24ccc91c6
AbdAlQaderFouadFarajallah <aboodfouad2000@gmail.com>
2023-02-01 11:48:06 +0200

    add launch screen

---
+++ lib/screens/launch_screen.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+import '../controllers/fb_auth_controller.dart';
+
+class LaunchScreen extends StatefulWidget {
+  const LaunchScreen({Key? key}) : super(key: key);
+
+  @override
+  _LaunchScreenState createState() => _LaunchScreenState();
+}
+
+class _LaunchScreenState extends State<LaunchScreen> {
+  late StreamSubscription _streamSubscription;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      _streamSubscription = FbAuthController().checkUserState(
+        listener: ({required bool status}) {
+          String route = status ? '/notes_screen' : '/login_screen';
+          Navigator.pushReplacementNamed(context, route);
+        },
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _streamSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: AlignmentDirectional.topStart,
+            end: AlignmentDirectional.bottomEnd,
+            colors: [
+              Colors.blue.shade800,
+              Colors.orange.shade600,
+            ],
+          ),
+        ),
+        child: const Text(
+          'FIREBASE APP',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
